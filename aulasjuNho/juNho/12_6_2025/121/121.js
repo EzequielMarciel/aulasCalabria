6682b869d7e91548585a128c3660530b720bafdc
EzequielMarciel <ecarvalhomarciel>
2025-07-02 09:34:37 -0300

    aulas de junho

--- aulasjuNho/juNho/12_6_2025/121/121.js
+++
@@ -1,7 +0,0 @@
-function aplicarFiltro(filtro) {
-    const imagens = document.querySelectorAll('.galeria img');
-    imagens.forEach(img => {
-        img.style.filter = filtro
-    });
-}
-aplicarFiltro();
